5d48ee7790df1ac7efe2e2af1dc97bb686d854d3
Osama Heikal <109473076+OssHeikal@users.noreply.github.com>
2023-12-10 14:25:12 +0200

    fix: remove cubit from feature template

--- bricks/feature/__brick__/{{featureName.snakeCase()}}/view_models/{{featureName.snakeCase()}}_cubit/{{featureName.snakeCase()}}_state.dart
+++
@@ -1,9 +0,0 @@
-part of '{{featureName.snakeCase()}}_cubit.dart';
-
-class {{featureName.pascalCase()}}State extends Equatable {
-  const {{featureName.pascalCase()}}State();
-
-  @override
-  List<Object> get props => [];
-}
-
