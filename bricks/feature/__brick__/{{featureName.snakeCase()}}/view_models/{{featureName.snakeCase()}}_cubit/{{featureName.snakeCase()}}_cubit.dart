bc9b97b5cfdb0a46da0b5c9810ca7dd61e00ea0f
Osama Heikal <109473076+OssHeikal@users.noreply.github.com>
2023-12-10 14:19:31 +0200

    fix: fix feature view model folder name in feature bricks

--- bricks/feature/__brick__/{{featureName.snakeCase()}}/view_models/{{featureName.snakeCase()}}_cubit/{{featureName.snakeCase()}}_cubit.dart
+++ bricks/feature/__brick__/{{featureName.snakeCase()}}/view_models/{{featureName.snakeCase()}}_cubit/{{featureName.snakeCase()}}_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
-import '../../repository/{{featureName.snakeCase()}}_repository.dart';
+import "../../repository/{{featureName.snakeCase()}}_repository.dart";
 
 part '{{featureName.snakeCase()}}_state.dart';
 
